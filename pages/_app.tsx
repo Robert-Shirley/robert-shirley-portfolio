b056804fd0c1cc557fc4ec33ffa12165a49d17f3
Rob Shirley <rshirley67@gmail.com>
2024-08-20 12:39:06 -0600

    added layout and toaster

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
+import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+      <Toaster position="top-right" />
+    </Layout>
+  );
 }
