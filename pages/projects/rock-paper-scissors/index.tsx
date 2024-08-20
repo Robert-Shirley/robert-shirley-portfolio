333d4c398b579d0ce4a479ef1224411546d1f2b4
Rob Shirley <rshirley67@gmail.com>
2024-08-20 12:41:15 -0600

    index page for rps

---
+++ pages/projects/rock-paper-scissors/index.tsx
@@ -0,0 +1,11 @@
+import RockPaperScissors from "@/components/RockPaperScissors/RockPaperScissors";
+
+const Index = () => {
+  return (
+    <div>
+      <RockPaperScissors isComponent={false} />
+    </div>
+  );
+};
+
+export default Index;
