cf50fa7a27e3ecc168610a854a7e8f9189a0d63d
Rob Shirley <rshirley67@gmail.com>
2025-01-16 15:49:37 -0700

    wrap app in context for cart management

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import Layout from "@/components/Layout";
+import { CartProvider } from "@/context/CartContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-      <Toaster position="top-right" />
-    </Layout>
+    <CartProvider>
+      <Layout>
+        <Component {...pageProps} />
+        <Toaster position="top-left" />
+      </Layout>
+    </CartProvider>
   );
 }
