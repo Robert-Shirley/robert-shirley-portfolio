552636897df707477261aa7a42df64e74461e438
Rob Shirley <rshirley67@gmail.com>
2025-01-16 15:49:26 -0700

    added ecommerce site and updated some descriptions

--- data/projects.tsx
+++ data/projects.tsx
@@ -8,9 +8,73 @@ import WeatherApp from "@/components/WeatherApp/WeatherApp";
 
 const projects = [
   {
+    id: 0,
+    name: "E-Commerce Site",
+    description:
+      "This E-Commerce site is built with a modern tech stack designed for simplicity and responsiveness. The project leverages React and a custom useContext wrapper to manage global state (realtime updates to the cart from any page), ensuring efficient and scalable state management. Data is persisted through a custom local storage hook, so you won't lose your cart on a page refresh. Styling is handled with TailwindCSS, providing a sleek and responsive design. Data is fetched dynamically from the FakeStoreAPI, allowing for realistic mock data integration. The site uses Next.js for dynamic routing, enabling smooth navigation and SEO optimization. Features like interactive toasts from React Hot Toast enhance the user experience by providing real-time feedback. Overall, this stack ensures a robust and user-friendly application that's easy to extend and maintain.",
+    link: "/projects/ecommerce",
+    odinLink: "https://www.theodinproject.com/lessons/foundations-recipes",
+    images: [
+      "/images/ecommerce/Ecommerce1.png",
+      "/images/ecommerce/Ecommerce2.png",
+      "/images/ecommerce/Ecommerce3.png",
+      "/images/ecommerce/Ecommerce4.png",
+      "/images/ecommerce/Ecommerce5.png",
+      "/images/ecommerce/Ecommerce6.png",
+      "/images/ecommerce/Ecommerce7.png",
+      "/images/ecommerce/Ecommerce8.png",
+      "/images/ecommerce/Ecommerce9.png",
+    ],
+    showImages: true,
+    showComponent: false,
+    component: null,
+    techStack: [
+      "NextJS",
+      "TailwindCSS",
+      "NextJS Router",
+      "TypeScript",
+      "NextJS Image",
+      "React Hot Toast",
+      "React Context",
+      "React Hooks",
+      "FakeStoreAPI",
+      "Dynamic Routing",
+      "SEO Optimization",
+      "Cart Functionality",
+    ],
+  },
+  {
+    id: 12,
+    name: "CV Creator",
+    description:
+      "A dynamic CV creator app built with React Hook Form. Users can create customizable CVs by adding sections for personal details, skills, education, experience, projects, and certifications. The app integrates features like React Hook Form for efficient form handling and React To Print for seamless print functionality. TailwindCSS ensures a responsive and visually appealing design, while components and hooks streamline the app’s modular structure. The customization options include real-time changes to the header's font, size, and background color, showcasing attention to both functionality and user experience.",
+    link: "/projects/cv-application",
+    odinLink:
+      "https://www.theodinproject.com/lessons/node-path-react-new-cv-application",
+    images: [
+      "/images/cvApplication/cvAppIndex.png",
+      "/images/cvApplication/cvAppIndex2.png",
+      "/images/cvApplication/cvAppIndex3.png",
+    ],
+    showImages: true,
+    showComponent: false,
+    component: null,
+    techStack: [
+      "NextJS",
+      "TailwindCSS",
+      "React Hook Form",
+      "React To Print",
+      "TypeScript",
+      "React Components",
+      "React Hooks",
+      "Date Fns",
+    ],
+  },
+  {
     id: 1,
     name: "Recipes",
-    description: "A simple recipe app with images, lists, and navigation",
+    description:
+      "A recipe app that lets you browse and view recipes with images, ingredient lists, and step-by-step instructions. Built using NextJS, the app leverages static generation for fast performance and Next.js Image for optimized image loading. The clean design, powered by TailwindCSS, ensures an easy and enjoyable browsing experience.",
     link: "/projects/recipes",
     odinLink: "https://www.theodinproject.com/lessons/foundations-recipes",
     images: [
@@ -34,7 +98,7 @@ const projects = [
     id: 2,
     name: "Landing Page",
     description:
-      "A simple landing page with a header/navbar, hero section, images, quote, call to action, and a footer.",
+      "A clean and responsive landing page featuring modular React components such as a header with navigation, a hero section, a motivational quote, a call-to-action button, and a footer. The use of reusable components makes it easy to modify content and update the site’s structure, while TailwindCSS ensures consistent and modern styling. Optimized image handling enhances performance and provides a smooth user experience.",
     link: "/projects/landing-page",
     odinLink: "https://www.theodinproject.com/lessons/foundations-landing-page",
     images: [
@@ -55,7 +119,8 @@ const projects = [
   {
     id: 3,
     name: "Rock Paper Scissors",
-    description: "A simple rock paper scissors game with a win/loss counter",
+    description:
+      "A simple rock-paper-scissors game. The game uses React state to track wins, losses, and previous choices, while React Hot Toast provides real-time feedback for a better user experience.",
     link: "/projects/rock-paper-scissors",
     odinLink:
       "https://www.theodinproject.com/lessons/foundations-rock-paper-scissors",
@@ -76,7 +141,7 @@ const projects = [
     id: 4,
     name: "Etch a Sketch",
     description:
-      "A cool etch a sketch app with a customizable grid size, color mode, and mode toggle (coloring/not coloring)",
+      "A feature-packed Etch-a-Sketch app with customizable grid size and multiple drawing modes, including black-and-white, grayscale, random colors, and an eraser. The app also includes a toggleable coloring mode, allowing users to easily enable or disable coloring with a click.",
     link: "/projects/etch-a-sketch",
     odinLink:
       "https://www.theodinproject.com/lessons/foundations-etch-a-sketch",
@@ -96,7 +161,7 @@ const projects = [
     id: 5,
     name: "Calculator",
     description:
-      "A sleek and responsive calculator app, designed with TailwindCSS, offering essential arithmetic operations and seamless decimal handling",
+      "A sleek and responsive calculator app designed with TailwindCSS, offering essential arithmetic operations and seamless decimal handling. Users can interact with the calculator by clicking on numbers and operators, or enable keypress support to use their keyboard for input, providing a flexible and intuitive experience.",
     link: "/projects/calculator",
     odinLink: "https://www.theodinproject.com/lessons/foundations-calculator",
     showComponent: true,
