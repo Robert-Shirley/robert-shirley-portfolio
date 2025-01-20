568a304afcfb7ed14b137d53eaeb83c54f729c91
Rob Shirley <rshirley67@gmail.com>
2025-01-20 16:43:53 -0700

    fetch from api and better error handling

--- src/components/Ecommerce/Main.tsx
+++ src/components/Ecommerce/Main.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import { useFetchData } from "@/hooks/useFetchData";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useMemo } from "react";
 
 type MainImageProps = {
   title: string;
@@ -19,9 +20,11 @@ const MainImage: React.FC<MainImageProps> = ({
 }) => {
   return (
     <Link href={route} className="block">
-      <div>
-        <h2 className="text-lg text-black font-semibold">{title}</h2>
-        <div className="b h-fit w-fit rounded-lg overflow-hidden">
+      <div className="flex flex-col items-center">
+        <h2 className="text-xl text-black font-semibold text-center pb-2">
+          {title}
+        </h2>
+        <div className="border h-fit w-fit rounded-lg overflow-hidden">
           <Image
             src={imageUrl}
             alt={title}
@@ -30,6 +33,7 @@ const MainImage: React.FC<MainImageProps> = ({
             quality={90}
           />
         </div>
+
         <p className="max-w-36 text-center text-xs text-gray-500 mt-2">
           {description}
         </p>
@@ -39,27 +43,53 @@ const MainImage: React.FC<MainImageProps> = ({
 };
 
 const Main: React.FC = () => {
-  const routes = [
-    {
-      title: "Clothing Store",
-      imageUrl: "/images/ecommerce/products/71YXzeOuslL._AC_UY879_.jpg", // Men's casual slim fit t-shirt
-      description: "Browse our collection of modern clothing and accessories",
-      route: "/projects/ecommerce/clothing",
-    },
-    {
-      title: "Jewelry Collection",
-      imageUrl:
-        "/images/ecommerce/products/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg", // Gold plated necklace
-      description: "Explore our curated selection of fine jewelry",
-      route: "/projects/ecommerce/jewelry",
-    },
-    {
-      title: "Electronics Hub",
-      imageUrl: "/images/ecommerce/products/71kWymZ+c+L._AC_SX679_.jpg", // Samsung gaming monitor
-      description: "Discover the latest in consumer electronics",
-      route: "/projects/ecommerce/electronics",
-    },
-  ];
+  const { data, isLoading, error } = useFetchData(
+    "/api/ecommerce/category",
+    ["categories"],
+    "GET"
+  );
+
+  const routes = useMemo(() => {
+    const categories = data as {
+      id: string;
+      slug: string;
+      name: string;
+      image: string;
+      description: string;
+      imageUrl: string;
+    }[];
+
+    if (categories) {
+      return categories.map((category) => ({
+        id: category.id,
+        title: category.name,
+        imageUrl: category.imageUrl,
+        description: category.description,
+        route: `/projects/ecommerce/${category.slug.toLowerCase()}`,
+      }));
+    }
+
+    return [];
+  }, [data]);
+
+  if (isLoading) {
+    return (
+      <div className="min-h-screen bg-gray-50 px-4 py-12">
+        <div className="text-center">Loading...</div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="min-h-screen bg-gray-50 px-4 py-12">
+        <div className="text-center">
+          Failed to fetch data
+          {error.message}
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="min-h-96 h-fit bg-white flex text-gray-700 lg:px-48 py-12 w-full justify-center">
@@ -67,7 +97,7 @@ const Main: React.FC = () => {
         <h1 className="text-center text-black text-3xl">Explore Our Stores</h1>
         <div className="w-full flex flex-col xl:flex-row items-center xl:justify-evenly mt-8 gap-8 xl:gap-4">
           {routes.map((route) => (
-            <MainImage key={route.title} {...route} />
+            <MainImage key={route.id} {...route} />
           ))}
         </div>
       </div>
