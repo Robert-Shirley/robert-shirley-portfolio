5168094e1272b9ff5dbcb65ff1b6acb5c3178fa4
Rob Shirley <rshirley67@gmail.com>
2025-01-20 16:44:58 -0700

    created hook to fetch data with error and loading state

---
+++ src/hooks/useFetchData.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import {
+  QueryKey,
+  QueryObserverOptions,
+  useQuery,
+} from "@tanstack/react-query";
+
+type AllowedMethods = "GET" | "POST" | "PUT" | "DELETE";
+
+const fetchData = async <TBody, UResponse>(
+  url: string,
+  method?: AllowedMethods,
+  body?: TBody
+): Promise<UResponse> => {
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  const requestInit: RequestInit = {
+    method: method || "GET",
+    headers,
+  };
+  if (method !== "GET") requestInit.body = JSON.stringify(body);
+
+  const response = await fetch(url, requestInit);
+
+  if (!response.ok) {
+    try {
+      const responseJson = await response.json();
+      throw new Error(
+        responseJson.error || responseJson.message || "Request failed"
+      );
+    } catch {
+      throw new Error("Request failed");
+    }
+  }
+
+  return response.json();
+};
+
+export const useFetchData = <TBody, UResponse>(
+  url: string,
+  queryKey: QueryKey,
+  method?: AllowedMethods,
+  body?: TBody,
+  queryOptions?: Partial<QueryObserverOptions<UResponse>>
+) => {
+  return useQuery<UResponse, Error>({
+    ...queryOptions,
+    queryKey: [...queryKey, JSON.stringify(body)],
+    queryFn: () => fetchData<TBody, UResponse>(url, method, body),
+  });
+};
